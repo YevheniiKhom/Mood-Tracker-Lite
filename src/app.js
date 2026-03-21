cbb6db232df6d586f470a851cc9a66dad73acc2d
unknown <zenahomenko80@gmail.com>
2026-03-21 13:00:17 +0100

    default

--- src/app.js
+++ src/app.js
@@ -1,31 +1,35 @@
-let happyBtn = document.querySelector("#happy-btn");
-let suprisedBtn = document.querySelector("#suprised-btn");
-let sadBtn = document.querySelector("#sad-btn");
 let saveMomentBtn = document.querySelector("#save-moment");
 let inputField = document.querySelector("#emotion");
 let notesText = document.querySelector("#notes-text");
+let moodBtn = document.querySelectorAll(".mood-btn");
 
-//  Рефакторинг  Зробити емоції однією командою
-happyBtn.addEventListener("click", () => {
-  event.preventDefault();
-  inputField.value += `\u{1F642}`;
-  inputField.focus();
-});
-suprisedBtn.addEventListener("click", () => {
-  event.preventDefault();
-  inputField.value += `\u{1F610}`;
-  inputField.focus();
-});
-sadBtn.addEventListener("click", () => {
-  event.preventDefault();
-  inputField.value += `\u{1F61F}`;
-  inputField.focus();
+moodBtn.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    inputField.value += btn.dataset.emoji;
+    inputField.focus();
+  });
 });
 
 saveMomentBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  let create = document.createElement("li");
-  create.textContent = inputField.value;
-  notesText.appendChild(create);
-  inputField.value = "";
+  if (inputField.value.trim()) {
+    let newLi = document.createElement("li");
+    let textSpan = document.createElement("span");
+    let deleteBtn = document.createElement("button");
+
+    textSpan.textContent = inputField.value;
+    textSpan.classList.add("note-content");
+
+    deleteBtn.textContent = "X";
+    deleteBtn.classList.add("delete-btn");
+    deleteBtn.addEventListener("click", () => newLi.remove());
+
+    newLi.appendChild(textSpan);
+    newLi.appendChild(deleteBtn);
+
+    notesText.prepend(newLi);
+
+    inputField.value = "";
+  }
 });
